30c656c9a49f543776295f84e361fff651912db4
Justin <j.70.robles@gmail.com>
2017-10-23 17:31:59 -0700

    Project done. Need to update README

--- acp_times.py
+++ acp_times.py
@@ -31,23 +31,53 @@ def open_time( control_dist_km, brevet_dist_km, brevet_start_time):
        This will be in the same time zone as the brevet start time.
     """
     # Add correct math for times
-    speeds = {
-                200: 15,
-                400: 15,
-                600: 15,
-                1000: 11.428,
-                1300: 13.333
+    max_speeds = {
+        200: 34,
+        400: 32,
+        600: 30,
+        1000: 28,
+        1300: 26
     }
-    if control_dist_km == 0:
-        return arrow.get(brevet_start_time).format("ddd M/D H:mm")
 
-    for min_speed in speeds:
-        if control_dist_km <= min_speed:
-            control_open = control_dist_km / speeds[min_speed]
-            hrs = math.floor(control_open)
-            mins = round((control_open - hrs) * 60)
-            rusa = "{}H{}".format(hrs, mins)
-            return arrow.get(brevet_start_time).shift(hours=hrs, minutes=mins).format("ddd M/D H:mm")
+    brevet_open_default = {
+        200: (5, 53),
+        300: (9, 0),
+        400: (12, 8),
+        600: (18, 48),
+        1000: (33, 5)
+    }
+    control_open = arrow.get(brevet_start_time)
+
+    if control_dist_km == 0:
+        return control_open.format("ddd M/D H:mm")
+
+    if (control_dist_km >= brevet_dist_km) & (control_dist_km <= (brevet_dist_km * 1.1)):
+        hrs, mins = brevet_open_default[brevet_dist_km]
+        return control_open.shift(hours=hrs, minutes=mins).format("ddd M/D H:mm")
+
+    if control_dist_km > (brevet_dist_km * 1.1):
+        return ""
+
+    control_dist_km = round(control_dist_km)
+    start_time = 0
+    first_key = next (iter (max_speeds.values()))
+    last_key = 0
+
+    for key in max_speeds:
+        if control_dist_km > key:
+            start_time += (key - last_key) / max_speeds[key]
+            last_key = key
+        else:
+            if control_dist_km <= first_key:
+                start_time += control_dist_km / first_key
+            else:
+                start_time += (control_dist_km - last_key) / max_speeds[key]
+            if start_time != 0:
+                hrs = math.floor(start_time)
+                mins = round((start_time - hrs) * 60)
+                rusa = "{}H{}".format(hrs, mins)
+                return control_open.shift(hours=hrs, minutes=mins).format("ddd M/D H:mm")
+
 
 
 def close_time( control_dist_km, brevet_dist_km, brevet_start_time ):
@@ -62,7 +92,56 @@ def close_time( control_dist_km, brevet_dist_km, brevet_start_time ):
     Returns:
        An ISO 8601 format date string indicating the control close time.
        This will be in the same time zone as the brevet start time.
+       13:30 for 200 KM, 20:00 for 300 KM, 27:00 for 400 KM, 40:00 for 600 KM, and 75:00 for 1000 KM
     """
-    return arrow.get(brevet_start_time).format("ddd M/D H:mm")
+
+    min_speeds = {
+        200: 15,
+        400: 15,
+        600: 15,
+        1000: 11.428,
+        1300: 13.333
+    }
+
+    brevet_close_default = {
+        200: (13, 30),
+        300: (20, 0),
+        400: (27, 0),
+        600: (40, 0),
+        1000: (75, 0)
+    }
+
+    control_close = arrow.get(brevet_start_time)
+
+    if control_dist_km == 0:
+        return arrow.get(brevet_start_time).shift(hours=1).format("ddd M/D H:mm")
+
+    if (control_dist_km >= brevet_dist_km) & (control_dist_km <= (brevet_dist_km * 1.1)):
+        hrs, mins = brevet_close_default[brevet_dist_km]
+        return control_close.shift(hours=hrs, minutes=mins).format("ddd M/D H:mm")
+
+    if control_dist_km > (brevet_dist_km * 1.1):
+        return ""
+
+    control_dist_km = round(control_dist_km)
 
 
+    start_time = 0
+    first_key = next (iter (min_speeds.values()))
+    last_key = 0
+
+    for key in min_speeds:
+        if control_dist_km > key:
+            start_time += (key - last_key) / min_speeds[key]
+            last_key = key
+        else:
+            if control_dist_km <= first_key:
+                start_time += control_dist_km / first_key
+            else:
+                start_time += (control_dist_km - last_key) / min_speeds[key]
+            if start_time != 0:
+                hrs = math.floor(start_time)
+                mins = round((start_time - hrs) * 60)
+                rusa = "{}H{}".format(hrs, mins)
+                print("HOURS: {} MINS: {}, RUSA: {}, STARTTIME: {}".format(hrs, mins, rusa, start_time))
+                return control_close.shift(hours=hrs, minutes=mins).format("ddd M/D H:mm")
--- flask_controls.py
+++ flask_controls.py
@@ -71,7 +71,7 @@ def _calc_times():
   #FIXME: These probably aren't the right open and close times
   open_time = acp_times.open_time(km, brevet, arrow.get(date).isoformat())
   close_time = acp_times.close_time(km, brevet, arrow.get(date).isoformat())
-  result={ "open": open_time, "close": close_time }
+  result={ "open": open_time, "close": close_time,  }
   return jsonify(result=result)
 
 
